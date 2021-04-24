14d84aa0364162b4646fc2a8f5ed0c220f96dbd3
Armando <armandotechno12@gmail.com>
2021-04-24 13:40:19 -0400

    Cargando un componente u otro

--- src/App.js
+++ src/App.js
@@ -7,27 +7,37 @@ function App() {
   //definir el state
   const [ presupuesto, guardarPresupuesto] = useState(0);
   const [ restante, guardarRestante] = useState(0);
+  const [mostrarpregunta, actualizarPregunta] = useState(true);
 
 
   return (
     <div className="Container">
+      
       <header>
           <h1>Gasto Semanal</h1>
 
           <div className="contenido-principal contenido">
+            {mostrarpregunta ? 
+            (
               <Pregunta 
                 guardarPresupuesto={guardarPresupuesto}
                 guardarRestante={guardarRestante}
+                actualizarPregunta={actualizarPregunta}
               />
-
+            ): (
               <div className="row">
                 <div className="one-half column">
                   <Formulario />
                 </div>
                 <div className="one-half column">
-                  2
-                </div>
+                2
               </div>
+            </div> 
+              )
+            }
+              
+
+              
           </div>
       </header>
     </div>
